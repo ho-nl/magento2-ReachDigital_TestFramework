4ee30cb7beec161df1174d4145b63c4d77fa37dd
jisse Reitsma <jisse@yireo.com>
2020-11-17 14:39:56 +0100

    Better workaround for TestSetter

--- framework/ReachDigital/TestFramework/Bootstrap/DocBlock.php
+++ framework/ReachDigital/TestFramework/Bootstrap/DocBlock.php
@@ -17,6 +17,13 @@ class DocBlock extends \Magento\TestFramework\Bootstrap\DocBlock
 //            new \Magento\TestFramework\Workaround\Cleanup\StaticProperties(),
             new \Magento\TestFramework\Isolation\WorkingDirectory(),
             new \Magento\TestFramework\Isolation\DeploymentConfig(),
+        ];
+
+        if (class_exists(\Magento\TestFramework\Workaround\Override\Fixture\Resolver\TestSetter::class)) {
+            $subscribers[] = new \Magento\TestFramework\Workaround\Override\Fixture\Resolver\TestSetter();
+        }
+
+        $subscribers = array_merge($subscribers, [
             new \ReachDigital\TestFramework\Annotation\AppIsolation($application),
             new \Magento\TestFramework\Annotation\IndexerDimensionMode($application),
             new \Magento\TestFramework\Isolation\AppConfig(),
@@ -35,11 +42,7 @@ class DocBlock extends \Magento\TestFramework\Bootstrap\DocBlock
             new \Magento\TestFramework\Annotation\Cache($application),
             new \Magento\TestFramework\Annotation\AdminConfigFixture(),
             new \Magento\TestFramework\Annotation\ConfigFixture(),
-        ];
-
-        if (class_exists(\Magento\TestFramework\Workaround\Override\Fixture\Resolver\TestSetter::class)) {
-            $subscribers[] = new \Magento\TestFramework\Workaround\Override\Fixture\Resolver\TestSetter();
-        }
+        ]);
 
         return $subscribers;
     }
